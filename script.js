07bf83235a40b5e9e75e9a1b2081b452d37543c8
Kevin Getz <kgetz32@gmail.com>
2022-08-23 13:21:27 -0700

    added div to dom

---
+++ script.js
@@ -0,0 +1,7 @@
+// select squaresContainer to put divs in
+const container = document.querySelector('.squaresContainer');
+// creat div and append to container
+const squareDiv = document.createElement('div');
+squareDiv.classList.add("eachSquare");
+container.appendChild(squareDiv);
+
