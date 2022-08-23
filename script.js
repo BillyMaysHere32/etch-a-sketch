8d94db5e98f0db653d813cf6abeba6e1cb5ec7a6
Kevin Getz <kgetz32@gmail.com>
2022-08-23 14:50:14 -0700

    created grid onjs and added cs styling

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // select squaresContainer to put divs in
 const container = document.querySelector('.squaresContainer');
-// creat div and append to container
-const squareDiv = document.createElement('div');
-squareDiv.classList.add("eachSquare");
-container.appendChild(squareDiv);
 
+function createGrid() {
+    for (let i = 0; i < 256; i++) {
+
+    // creat div and append to container
+    const squareDiv = document.createElement('div');
+    squareDiv.classList.add("eachSquare");
+
+    container.appendChild(squareDiv);
+    };
+}
+
+createGrid();
