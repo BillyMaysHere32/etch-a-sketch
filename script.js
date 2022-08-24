b9fc751544a62b6dea1b95f844059886149bedf8
Kevin Getz <kgetz32@gmail.com>
2022-08-24 10:10:20 -0700

    added if statement so grid max is 100 and prompt error is over

--- script.js
+++ script.js
@@ -28,20 +28,24 @@ function newGrid() {
     container.innerHTML = "";
     let newGridCountSquared = newGridCount * newGridCount;
     console.log(newGridCountSquared); 
-    for (let i = 0; i < newGridCountSquared; i++) {
-        // creat div and append to container
-        const squareDiv = document.createElement('div');
-        squareDiv.classList.add("eachSquare");
-        container.appendChild(squareDiv);
-        };
-    
-        container.style.setProperty("grid-template-columns",
-        `repeat(${newGridCount}, 2fr)`);
-        console.log(newGridCount);
+    if (newGridCount <= 100) {
+        for (let i = 0; i < newGridCountSquared; i++) {
+            // creat div and append to container
+            const squareDiv = document.createElement('div');
+            squareDiv.classList.add("eachSquare");
+            container.appendChild(squareDiv);
+            };
+        
+            container.style.setProperty("grid-template-columns",
+            `repeat(${newGridCount}, 2fr)`);
+            console.log(newGridCount);
 
 
-        container.style.setProperty("grid-template-rows",
-        `repeat(${newGridCount}, 2fr)`);
+            container.style.setProperty("grid-template-rows",
+            `repeat(${newGridCount}, 2fr)`);
+        } else {
+            return alert("Error, please enter a number from 1-100");
+        }
 }
 newGrid();
 });   
